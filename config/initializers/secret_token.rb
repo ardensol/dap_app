3e27e2769356ed7e283ef22fbdf54db1cbdb1b22
Carlos Solorio <csolorio05@gmail.com>
2014-06-24 21:11:48 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DapApp::Application.config.secret_key_base = '7f251f4f2244480f973cbf7cbce5a02ac01c0d376e6fcbea8fa0207caa6f4e1ad279f25bd3f250343d541ca0aceceb11e904ee86425908ee29ca661b36232220'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DapApp::Application.config.session_store :cookie_store, key: '_dap_app_session'
